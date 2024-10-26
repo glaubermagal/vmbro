af26f25f65d2f4e2e5010d82950fff585c8a3a17
Glauber Magalhães <glauber.tecinfo@gmail.com>
2024-10-26 15:33:43 -0300

    disable experimental message

--- app.py
+++ app.py
@@ -11,15 +11,17 @@ driver = dict()
 def open_browser():
     global driver
 
-    # if driver is None:
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")  # For Linux users
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-local-file-accesses")
+
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
     
     uid = str(uuid.uuid4())
     driver[uid] = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        
-    # Open a specific URL
+    
     driver[uid].get("about:blank")
 
     return jsonify({
@@ -32,8 +34,7 @@ def close_browser(uid):
     if uid in driver.keys():
         driver[uid].quit()
         del driver[uid]
-        # driver[uid] = None
-    return jsonify({"status": "Browser closed"})
+    return jsonify({"status": "closed"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
