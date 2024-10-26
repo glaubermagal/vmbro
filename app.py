020b78e6c73a2c67a2b636f11ad0ba1b9aeef75b
Glauber Magalhães <glauber.tecinfo@gmail.com>
2024-10-26 15:26:13 -0300

    open and close browser based on session

--- app.py
+++ app.py
@@ -2,32 +2,37 @@ from flask import Flask, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import uuid
 
 app = Flask(__name__)
-driver = None
+driver = dict()
 
 @app.route('/open-browser')
 def open_browser():
     global driver
-    if driver is None:
-        options = webdriver.ChromeOptions()
-        options.add_argument("--no-sandbox")  # For Linux users
-        options.add_argument("--disable-dev-shm-usage")
-        
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    # if driver is None:
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")  # For Linux users
+    options.add_argument("--disable-dev-shm-usage")
+    
+    uid = str(uuid.uuid4())
+    driver[uid] = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         
     # Open a specific URL
-    driver.get("about://blank")
-    page_title = driver.title
+    driver[uid].get("about:blank")
 
-    return jsonify({"title": page_title})
+    return jsonify({
+        "uid": uid
+    })
 
-@app.route('/close-browser')
-def close_browser():
+@app.route('/close-browser/<path:uid>')
+def close_browser(uid):
     global driver
-    if driver:
-        driver.quit()
-        driver = None
+    if uid in driver.keys():
+        driver[uid].quit()
+        del driver[uid]
+        # driver[uid] = None
     return jsonify({"status": "Browser closed"})
 
 if __name__ == '__main__':
